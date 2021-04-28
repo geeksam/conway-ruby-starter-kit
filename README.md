419f8c350f11073bb9c9b69b0001b6ed778ee9ec
Sam Livingston-Gray <geeksam@gmail.com>
2021-04-28 12:41:04 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# THURSDAY:  evil ping-pong pairing
+
+Just did a few minutes of setup in advance :)
---
+++ spec/conway_spec.rb
@@ -0,0 +1,10 @@
+require_relative 'spec_helper'
+
+RSpec.describe "the game of life (don't talk to me about life)" do
+
+  specify "the tests run" do
+    expect( true ).to     be true
+    expect( true ).to_not be false
+  end
+
+end
