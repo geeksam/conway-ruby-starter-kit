9710f7076c62ed7ca57b3362bfa779ef8dedc88f
Sam Livingston-Gray <geeksam@gmail.com>
2021-10-26 09:55:40 -1000

    put the rules in the spec file, drink 'em both up

--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,5 +1,21 @@
 require_relative 'spec_helper'
 
+=begin
+
+  - Births: Each dead cell adjacent to exactly three live neighbors will become
+    live in the next generation.
+
+  - Death by isolation: Each live cell with one or fewer live neighbors will die
+    in the next generation.
+
+  - Death by overcrowding: Each live cell with four or more live neighbors will
+    die in the next generation.
+
+  - Survival: Each live cell with either two or three live neighbors will remain
+    alive for the next generation.
+
+=end
+
 RSpec.describe "the game of life (don't talk to me about life)" do
 
   specify "the tests run" do
